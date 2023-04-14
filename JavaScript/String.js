e7205e7578d91282e81191a64f577a2196d5a49b
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-04-14 17:39:26 +0530

    14-4-2023

---
+++ JavaScript/String.js
@@ -0,0 +1,60 @@
+
+
+//String is a sequence of characters//
+
+var str = "Hello my name is Prashant";
+
+console.log(str);
+
+console.log(str[10]);
+
+console.log(str.length);
+
+                   //Methods -->
+
+              //Extractions Methods
+
+//  SLICE method    slice(start idx , end idx);
+
+let slicestr = str.slice(10,25);
+console.log(slicestr);
+
+let slicestr2 = str.slice(17); //17 idx ke baaad ka sb cut hoga 
+console.log(slicestr2);
+
+//  SUB-STRING method    substr(start idx , end idx);
+
+let ans = str.substr(10 , 19);
+console.log(ans);
+console.log(str);
+
+              // Replacing --->
+str = "Prashant Bhardwaj"; 
+console.log(str);
+
+
+      // upper case and lower case changing
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());      
+
+
+//  Concatination Method --->
+
+let fstr = "You guys ";
+let sstr = "are Smart ";
+
+let Concatinatedstr = fstr + sstr;
+console.log(Concatinatedstr);
+
+let concatstr = fstr.concat(sstr , "are NOT?");
+console.log(concatstr);
+
+
+//  Trim Method ---->  (Remove all the whitespaces)
+
+let trimstr = "     Hello";
+console.log(trimstr.length);
+console.log(trimstr.trim());
+trimstr = trimstr.trim();
+console.log(trimstr.length);
