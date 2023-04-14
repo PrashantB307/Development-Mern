768565019e2f3ca1e2d7243874887d60dd3a554a
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-04-14 17:38:38 +0530

    14-4-2023

---
+++ JavaScript/Demo.js
@@ -0,0 +1,17 @@
+let a;
+a=10;
+console.log(a);
+console.log(typeof a);
+
+a="Hello";
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+
+var num = 200;
+console.log(`Half of ${num} is ${num/2}`);
+
