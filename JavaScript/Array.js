7c2f0c1f80dc556bc64800769914dfadd563686c
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-04-14 17:38:13 +0530

    14-4-2023

---
+++ JavaScript/Array.js
@@ -0,0 +1,21 @@
+
+// Array is Collection of elements //
+
+let cars = ["BMW" , "AUDI" , "MG" , "TATA"];
+console.log(cars);
+
+//2D Array//
+
+let arr2d = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+console.table(arr2d); // for printing in table//
+
+console.log(arr2d[1][2]);
+arr2d[1][2]=10;
+arr2d[2][2]="pawan";
+console.table(arr2d);
+
+console.log(arr2d.length);
