dc694e00524b1907049c711ef345ddbe276ecd8b
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-04-18 22:51:21 +0530

    18-4-2023

---
+++ JavaScript/Closures.js
@@ -0,0 +1,14 @@
+function outer() {
+    var a = 7;
+    function inner() {
+        console.log(a);
+    }
+    a = 10;
+    return inner;
+    
+}
+
+var c = outer();
+console.log(c);
+c();
+// [Function: inner]  &  10
