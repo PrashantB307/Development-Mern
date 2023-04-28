14b42e76a0ec8b7c406d3ca50d47e62bc0c5c90f
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-04-29 00:08:45 +0530

    29-4-2023

---
+++ NodeJS/Automation/HackerRank/HackerRank.js
@@ -0,0 +1,10 @@
+const puppeteer = require("puppeteer");
+
+let browserOpenPromise = puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"]
+});
+browserOpenPromise.then(function() {
+    console.log("Browser is open");
+})
