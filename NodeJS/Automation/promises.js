684c71bbda95a6ff5f6a506fb617fe57ece73758
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-04-28 23:28:16 +0530

    Promises in Node JS

--- NodeJS/Automation/promises.js
+++ NodeJS/Automation/promises.js
@@ -1,12 +1,40 @@
 const fs = require('fs');
 
 console.log("before");
-fs.readFile("f1.txt", cb);
-function cb(err, data){
-    if(err){
-        console.log(err);
-    }
-    else console.log(data + "");
-}
+
+//---> Synchronous Working <----
+//==============================
+
+//let data = fs.readFileSync("f1.txt");
+//console.log(data + "");
+
+//----> Asynchronous Working<-----
+//================================
+
+// fs.readFile("f1.txt", cb);
+// // function cb(err, data){
+// //     if(err){
+// //         console.log(err);
+// //     }
+// //     else console.log(data + "");
+// // }
+
+
+// Promising Working --->
+//=======================
+
+let promiseThatFileRead = fs.promises.readFile("f1.txt");
+console.log(promiseThatFileRead);
+promiseThatFileRead.then(printData);
+promiseThatFileRead.catch(printError);
 
 console.log("after");
+
+function printData(data){
+    console.log("Promise is Fulfilled");
+    console.log(data + "");
+}
+
+function printError(err){
+    console.log(err);
+}
