977f32bc930f29427b05239dd7cafb136ca8528f
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-05 22:39:22 +0530

    5-5-2023

--- NodeJS/Automation/POC/setTimeOut.js
+++ NodeJS/Automation/POC/setTimeOut.js
@@ -1,46 +1,46 @@
-// {
-//     function yolo() {
-//         var a = 10;
-//         function cb (err, res) {
-//             if(err){
-//                 console.log(err);
-//             }
-//             else{
-//                 console.log("Hello How are You ?");
-//             }
+{
+    function yolo() {
+        var a = 10;
+        function cb (err, res) {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Hello How are You ?");
+            }
            
-//         }
-//         // function to be executed ( CallBack fun) , time(mili sec.)
-//         //after which cb fun.  to be axecuted.
-//         setTimeout(cb, 4000);  // 4000 ms  ==> 4sec
-//         console.log(a);
-//     }
+        }
+        // function to be executed ( CallBack fun) , time(mili sec.)
+        //after which cb fun.  to be axecuted.
+        setTimeout(cb, 4000);  // 4000 ms  ==> 4sec
+        console.log(a);
+    }
     
-//     yolo();
+    yolo();
     
-//     let b = 100;
-//     console.log(b);
-// }
+    let b = 100;
+    console.log(b);
+}
 
-// const request = require("request");
-// let url = "https://www.worldometers.info/coronavirus/";
-// function yolo () {
-//     var a = 10;
-//     function cb(err, res) {
-//         if(err){
-//             console.log(err);
-//         }else{
-//             console.log("Hello how are you ?");
-//             console.log("statusCode is ");
-//         }
-//     }
-//     request(url, cb);
-//     console.log(a);
-// }
+const request = require("request");
+let url = "https://www.worldometers.info/coronavirus/";
+function yolo () {
+    var a = 10;
+    function cb(err, res) {
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Hello how are you ?");
+            console.log("statusCode is ");
+        }
+    }
+    request(url, cb);
+    console.log(a);
+}
 
-// yolo();
-// let b = 100;
-// console.log(b);
+yolo();
+let b = 100;
+console.log(b);
 
 //<===========================================================>
 
@@ -48,24 +48,24 @@
 //<============= Print 1 to 10 with 2 sec. gapping ===========>
 
 
-// for(let i = 1; i <= 10; i++){
-//     setTimeout(function () {
-//         console.log(i);
-//     }, 2000*i);
-// }
+for(let i = 1; i <= 10; i++){
+    setTimeout(function () {
+        console.log(i);
+    }, 2000*i);
+}
 
-// //<================ Another Way (Var) ==================>
+//<================ Another Way (Var) ==================>
 
-// for(var i = 1; i <= 10; i++){
-//     function outer(a) {
-//         setTimeout(function () {
-//             console.log(a);
-//         }, 2000 * a);
+for(var i = 1; i <= 10; i++){
+    function outer(a) {
+        setTimeout(function () {
+            console.log(a);
+        }, 2000 * a);
             
-//     }
+    }
 
-//     outer(i);
-// }
+    outer(i);
+}
 
 //<================ Another Way (Var) ==================>
 
@@ -80,14 +80,14 @@ for(var i = 1; i <= 10; i++){
 //<=======================================================>
 
 
-// function a() {
-//     let x = 100;
-//     function y() {
-//         return x;
-//     }
-//     return y;
-// }
+function a() {
+    let x = 100;
+    function y() {
+        return x;
+    }
+    return y;
+}
 
-// let fn = a();
-// let ans = fn();
-// console.log(ans); ///====>  100
+let fn = a();
+let ans = fn();
+console.log(ans); ///====>  100
