50cc67eec82b84371a860a645f683ccad892aed4
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-07 17:27:12 +0530

    7-5-2023

--- NodeJS/Automation/POC/CallBack.js
+++ NodeJS/Automation/POC/CallBack.js
@@ -94,3 +94,36 @@ function cb(err, res){
       console.log(res + "");
    }
 }
+
+
+
+// 3rd way is use Promises.
+//------------------------
+
+const fs = require("fs");
+
+let f1readPromise = fs.promises.readFile("f1.txt");
+
+function cb1(data) {
+   console.log(data + "");
+   let f2readPromise = fs.promises.readFile("f2.txt");
+   return f2readPromise;
+}
+
+function cb2(data) {
+   console.log(data + "");
+   let f3readPromise = fs.promises.readFile("f3.txt");
+   return f3readPromise;
+}
+function cb3(data) {
+   console.log(data + "");
+  console.log("Read all files.");
+}
+
+f1readPromise
+.then(cb1)
+.then(cb2)
+.then(cb3)
+.catch(function (err) {
+   console.log(err)
+});
