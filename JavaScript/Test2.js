9e4b38d76950789a133427f67cc3b4888e395291
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-08 22:15:25 +0530

    8-5-2023

--- JavaScript/Test2.js
+++ JavaScript/Test2.js
@@ -1,100 +1,100 @@
 //<--------------- Question 1 ------------------>
 //===============================================
 
-// const num = 10;
-// console.log(num + 5);   //===> 15
-// let a = 6;
-// a = a + num;
-// console.log(num - a);   //===> -6
+const num = 10;
+console.log(num + 5);   //===> 15
+let a = 6;
+a = a + num;
+console.log(num - a);   //===> -6
 
 
-// //<--------------- Question 2 ------------------>
-// //===============================================
+//<--------------- Question 2 ------------------>
+//===============================================
 
-// let a = 1;
-// { let a = 2;
-//     { let a = 3;
-//         { let a = 4;
-//             console.log(a);  //===> 4
-//         }
-//         console.log(a);  //===> 3
-//     }
-//     console.log(a);  //===> 2
-// }
-// console.log(a);  //===> 1
+let a = 1;
+{ let a = 2;
+    { let a = 3;
+        { let a = 4;
+            console.log(a);  //===> 4
+        }
+        console.log(a);  //===> 3
+    }
+    console.log(a);  //===> 2
+}
+console.log(a);  //===> 1
  
 
-// //<--------------- Question 3 ------------------>
-// //===============================================
+//<--------------- Question 3 ------------------>
+//===============================================
 
-// var num1 = 10;
-// let num2 = 10;
-// {
-//     var num1 = 20;
-//     let num2 = 20;
-// }
-// console.log("num1:", num1, "num2:", num2);
-// //===> num1: 20 num2: 10
+var num1 = 10;
+let num2 = 10;
+{
+    var num1 = 20;
+    let num2 = 20;
+}
+console.log("num1:", num1, "num2:", num2);
+//===> num1: 20 num2: 10
 
 
-// //<--------------- Question 4 ------------------>
-// //===============================================
+//<--------------- Question 4 ------------------>
+//===============================================
 
-// console.log("Line 4: ", a);  //===> Line 4:  undefined
-// var a;
-// console.log("Line 6: ", a);  //===> Line 6:  undefined
-// a = 10;
-// console.log("Line 8: ", a);  //===> Line 8:  10
+console.log("Line 4: ", a);  //===> Line 4:  undefined
+var a;
+console.log("Line 6: ", a);  //===> Line 6:  undefined
+a = 10;
+console.log("Line 8: ", a);  //===> Line 8:  10
  
 
-// //<--------------- Question 5 ------------------>
-// //===============================================
+//<--------------- Question 5 ------------------>
+//===============================================
 
-// const a = 10;
-// const b = 20;
-// b = b + 10;
-// console.log(a + b);  //===> Error (b cann't be re-inisilise)
-
-
-// //<--------------- Question 6 ------------------>
-// //===============================================
-
-// var a ="Bhardwaj";
-// var result = a.substring(3, 7);
-// console.log(result);   //===> rdwa
+const a = 10;
+const b = 20;
+b = b + 10;
+console.log(a + b);  //===> Error (b cann't be re-inisilise)
 
 
-// // <--------------- Question 7 ------------------>
-// // ===============================================
+//<--------------- Question 6 ------------------>
+//===============================================
 
-// var a = "hello";
-// var sum = 0;
-// for(var i = 0; i < a.length; i++){
-//     sum += (a[i] - "a");
-// }
-// console.log(sum);  //===> NaN
+var a ="Bhardwaj";
+var result = a.substring(3, 7);
+console.log(result);   //===> rdwa
 
 
-// //<--------------- Question 8 ------------------>
-// // ===============================================
+// <--------------- Question 7 ------------------>
+// ===============================================
 
-// let obj1 = {
-//     fName : "Pawan",
-//     sName : "Bhardwaj",
-//     age : 22,
-//     city : "Varanasi"
-// }
-// delete obj1.age;
-// console.log(obj1);
-// //===> { fName: 'Pawan', sName: 'Bhardwaj', city: 'Varanasi' }
+var a = "hello";
+var sum = 0;
+for(var i = 0; i < a.length; i++){
+    sum += (a[i] - "a");
+}
+console.log(sum);  //===> NaN
 
 
-// //<--------------- Question 9 ------------------>
-// // ===============================================
+//<--------------- Question 8 ------------------>
+// ===============================================
 
-// var a = 29;
-// var b = a = 30;
-// console.log(a);  //===> a = 30 
+let obj1 = {
+    fName : "Pawan",
+    sName : "Bhardwaj",
+    age : 22,
+    city : "Varanasi"
+}
+delete obj1.age;
+console.log(obj1);
+//===> { fName: 'Pawan', sName: 'Bhardwaj', city: 'Varanasi' }
+
+
+//<--------------- Question 9 ------------------>
+// ===============================================
+
+var a = 29;
+var b = a = 30;
+console.log(a);  //===> a = 30 
 
 
 //<--------------- Question 10 ------------------>
