ba993e2f5bff0e9824cbdac8140da333457853e5
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-08 19:32:29 +0530

    8-5-2023

--- JavaScript/Test2.js
+++ JavaScript/Test2.js
@@ -1,11 +1,11 @@
 //<--------------- Question 1 ------------------>
 //===============================================
 
-const num = 10;
-console.log(num + 5);   //===> 15
-let a = 6;
-a = a + num;
-console.log(num - a);   //===> -6
+// const num = 10;
+// console.log(num + 5);   //===> 15
+// let a = 6;
+// a = a + num;
+// console.log(num - a);   //===> -6
 
 
 // //<--------------- Question 2 ------------------>
@@ -26,3 +26,23 @@ console.log(num - a);   //===> -6
 
 //<--------------- Question 3 ------------------>
 //===============================================
+
+var num1 = 10;
+let num2 = 10;
+{
+    var num1 = 20;
+    let num2 = 20;
+}
+console.log("num1:", num1, "num2:", num2);
+//===> num1: 20 num2: 10
+
+
+//<--------------- Question 4 ------------------>
+//===============================================
+
+console.log("Line 4: ", a);  //===> Line 4:  undefined
+var a;
+console.log("Line 6: ", a);  //===> Line 6:  undefined
+a = 10;
+console.log("Line 8: ", a);  //===> Line 8:  10
+ 
