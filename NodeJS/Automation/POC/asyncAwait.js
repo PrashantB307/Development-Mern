9d6aa71f1d1e2791da005791b2e19512c0cd9471
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-09 15:07:46 +0530

    9-5-2023

--- NodeJS/Automation/POC/asyncAwait.js
+++ NodeJS/Automation/POC/asyncAwait.js
@@ -12,10 +12,34 @@ console.log(f());  //===> Promise { 'Hi' }
 
 
 
-//-------------------> asyncAwait <-------------------
+//-------------------> async <-----------------------
 //====================================================
 
 async function f() {
     return "Hello"
 }
 console.log(f());   //===> Promise { 'Hello' }
+
+
+
+//-------------------> await <------------------------
+//====================================================
+
+//===> The keyword await makes JS wait until the promise settles
+//     and return its results.
+//     ( Wait until the Promise resolve )
+
+async function f() {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout (() => resolve("done"), 5000)
+    });
+
+    let result = await promise;
+    console.log("Hello");
+    console.log(result);
+}
+f();
+console.log("YOLO");
+
+//================> YOLO
+//(after 5 sec) ==> Hello Done
