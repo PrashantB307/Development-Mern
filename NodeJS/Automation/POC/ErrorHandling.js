119fd2a240f17ae782a670f316f38924fb67a6d5
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-10 19:45:55 +0530

    10-5-2023

---
+++ NodeJS/Automation/POC/ErrorHandling.js
@@ -0,0 +1,21 @@
+// <--------------------- try_ statement -------------------->
+// ===========================================================
+
+// The statement to be executed.
+
+
+// <-------------------- catch_ statement ------------------->
+// ===========================================================
+
+// Statement that is executed if an exception is thrown in the 
+// try block.
+
+
+
+// <------------------- finally_ statement ------------------->
+// ============================================================
+
+// Statement that are executed after the try statement completed.
+// These statement executed regardless of whether an exception was
+// thrown or caught.
+
