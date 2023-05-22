a757cb3023c4e9e6c1a5276333639e19ae0b4473
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-22 11:55:36 +0530

    22-5-2023

---
+++ JavaScript/Jira/script.js
@@ -0,0 +1,6 @@
+const addBtn = document.querySelector(".add-btn");
+const modalCont = document.querySelector(".modal-cont");
+
+addBtn.addEventListener("click" , function() {
+    modalCont.style.display = "flex";
+})
