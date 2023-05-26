54d592daf54e979653a9a96e49271963d54c4533
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-26 09:54:23 +0530

    26-5-2023

--- JavaScript/Jira/script.js
+++ JavaScript/Jira/script.js
@@ -35,11 +35,17 @@ allPriorityColors.forEach(function (colorElement) {
 modalCont.addEventListener("keydown", function (e) {
     let key = e.key;
     if(key == "Shift"){
+        console.log(modalPriorityColor);
+        console.log(textAreaCont.value);
         createTicket(modalPriorityColor, textAreaCont.value);
         modalCont.style.display = "none";
         isModalPresent = false;
+        textAreaCont.value = "";
+        allPriorityColors.forEach(function (colorElem) {
+            colorElem.classList.remove("active");
+        })
     }
-});
+}); 
 
 function createTicket(ticketColor, data) {
     let ticketCont = document.createElement("div"); 
