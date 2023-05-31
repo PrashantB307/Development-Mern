2f6c2cab2f52e0d596fa3e7e839fe0a1322dca99
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-05-31 09:32:13 +0530

    31-5-2023

--- JavaScript/Interview Obj.js
+++ JavaScript/Interview Obj.js
@@ -41,3 +41,33 @@ console.log(clone);     //===>  { name: 'Bhardwaj', age: 22 }
 
 Object.assign(clone, user, {full_name: "Prashant Bhardwaj"});
 console.log(clone);     //===> { name: 'Pawan', age: 22, full_name: ' Prashant Bhardwaj' }
+
+
+
+//=====================> Nested Cloning <=======================
+//--------------------------------------------------------------
+
+let user1 = {
+    name : "Pawan",
+    age : 22,
+    sizes : {
+        height : 180,
+        weight : 70
+    }
+};
+
+console.log(user1.sizes);   //===>  { height: 180, weight: 70 }
+
+let clone1 = {};
+// This is not deep cloning , because reference to size identifier is same in
+// in both clone and user obj.
+
+Object.assign(clone1, user1);
+
+console.log(clone1);   //===>   { name: 'Pawan', age: 22, sizes: { height: 180, weight: 70 } }
+
+clone1.sizes.height = 165;
+// This is Deep cloning.
+
+console.log(clone1);   //===>   { name: 'Pawan', age: 22, sizes: { height: 165, weight: 70 } }
+console.log(user1);    //===>   { name: 'Pawan', age: 22, sizes: { height: 165, weight: 70 } }
