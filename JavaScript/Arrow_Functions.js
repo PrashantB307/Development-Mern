2548b6afb79d1082d1718ebadddeda0441f9ad7b
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-06 21:43:37 +0530

    6-6-2023

--- JavaScript/Arrow_Functions.js
+++ JavaScript/Arrow_Functions.js
@@ -11,3 +11,17 @@
 
 let sum = (a, b) => a + b;
 console.log( sum(1, 2) );    //=====>  3
+
+
+// *************************************************************************
+
+
+//  If we have only one argument, then parentheses around parameters can be
+//  omitted, making that even shorter.
+
+// For example: ---> 
+
+let double = n => n * 2;
+console.log( double(8) );    //=====>  16
+
+
