e52ecbb9ec72ffc6763fa6889417b3e8e520114f
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-12 08:12:26 +0530

    12-6-2023

---
+++ JavaScript/Distructuring.js
@@ -0,0 +1,27 @@
+
+
+//====================>   Array Destructuring   <====================
+//-------------------------------------------------------------------
+
+let name = ["Prashant", "Kumar", "Bhardwaj"];
+
+let [firstName, middlename] = name;
+console.log(firstName);    //=====> Prashant
+console.log(middlename);   //=====> Kumar
+
+let [ , , surname] = name;
+console.log(surname);      //=====> Bhardwaj
+
+
+let name2 = ["Hi", ["I", "am"], "P1"];
+let [a,b,c] = name2;
+
+console.log(a + " "+ b + " " + c);  // ===> Hi I,am P1
+
+
+let family = ["mummy"];
+let [mom, dad = "papa"] = family;  // here dad has default value
+console.log(mom);   //=====> mummy
+console.log(dad);   //=====> papa
+
+
