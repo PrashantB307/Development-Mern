c215c63031c6f672cf8aee01e421906e54f44637
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-16 11:24:55 +0530

    16-6-2023

---
+++ JavaScript/Camera_Gallery/script.js
@@ -0,0 +1,27 @@
+let video = document.querySelector("video");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn"); 
+let transparentColor = "transparent";
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+
+let recorder;
+let chunks = [];
+let constraints = {
+    video : true,
+    Audio : true
+};
+
+let shouldRecord = false; 
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then((stream) => {
+    video.srcObject = stream;
+
+    recorder = new MediaRecorder(stream);
+    recorder.addEventListener("start" , () => {
+        // Memory
+        chunks = [];
+        console.log("rec start");
+    })
+
