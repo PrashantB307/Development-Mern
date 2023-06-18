2f8c6b42dd04bde7fd2eba14959109338f4874ae
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-18 17:35:37 +0530

    18-6-2023

--- JavaScript/Camera_Gallery/script.js
+++ JavaScript/Camera_Gallery/script.js
@@ -76,4 +76,30 @@ captureBtnCont.addEventListener("click", () => {
 
     tool.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-   
+    // Applying filters on photos
+    tool.fillStyle = transparentColor;
+    tool.fillRect(0, 0, canvas.width, canvas.height);
+
+    let imageURL = canvas.toDataURL();
+    // let img = document.createElement("img");
+    // img.src = imageURL;
+    // document.body.append(img);
+    if(db) {
+        let ImageId = uid();
+        let dbTransaction = db.transaction("image", "readwrite");
+        let imageStore = dbTransaction.objectStore("image");
+        let imageEntry = {
+            id: `img-${ImageId}`,
+            url: imageURL
+        };
+        let addRequest = imageStore.add(imageEntry);
+        addRequest.onsuccess = () => {
+            console.log("Image added to db ");
+        }
+    }
+
+    setTimeout( () => {
+        captureBtn.classList.remove("scale-capture");
+    }, 510);
+});
+
