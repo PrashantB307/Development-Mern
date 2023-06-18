cdf2156867af989cfe0f5d8a0e06dadee659af3e
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-18 17:34:45 +0530

    18-6-2023

--- JavaScript/Camera_Gallery/script.js
+++ JavaScript/Camera_Gallery/script.js
@@ -31,4 +31,37 @@ navigator.mediaDevices.getUserMedia(constraints)
         console.log("rec start");
     });
 
-    
+    recorder.addEventListener("dataavailable" , (e) => {
+        chunks.push(e.data);
+        console.log("rec push in chunked");
+    });;
+
+    recorder.addEventListener("stop" , () => {
+        // convert video
+        let blob = new Blob(chunks, { type: 'video/mp4' });
+        console.log("rec stoped");
+        // Download video on desktop
+        let videoURL = URL.createObjectURL(blob);
+        console.log(videoURL);
+
+        // let a = document.createElement('a');
+        // a.href = videoURL;
+        // a.download = "myVideo.mp4";
+        // a.click();
+        // Store in database
+        if(db) {
+            let videoId = uid();
+            let dbTransaction = db.transaction("video", "readwrite");
+            let videoStore = dbTransaction.objectStore("video");
+            let videoEntry = {
+                id: `vid-${videoId}`,
+                blobData: blob,
+            };
+            let addRequest = videoStore.add(videoEntry);
+            addRequest.onsuccess = () => {
+                console.log("video added to db ");
+            };
+        }
+    });
+});
+
