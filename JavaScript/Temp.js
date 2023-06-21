f283f408c19355aa66637b273f6e180978829810
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-21 10:46:38 +0530

    21-6-2023

---
+++ JavaScript/Temp.js
@@ -0,0 +1,14 @@
+var obj = {
+    add : function add(a, b) {
+        return a + b;
+    },
+    sub : function subtract(a, b) {
+        return b - a;
+    },
+    hello : 123
+};
+
+var a = 10, b = 20;
+
+
+export default obj;
