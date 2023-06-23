47581115ef1c8aa94a9200fd84ec7fa8ef212dc0
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-23 15:32:34 +0530

    OOPS Concepts

--- JavaScript/Oops.js
+++ JavaScript/Oops.js
@@ -14,3 +14,47 @@ console.log(mypenguine);
 // =====> Penguine { habitat: 'Antartica' }
 
 
+//====================>  Inheritance  <=======================
+//------------------  "Extend" keyword  ----------------------
+
+
+class LivingOrganism {
+    constructor () {
+        this.breaths = true;
+    }
+
+    isBreathing () {
+        console.log(this.breaths);
+    }
+}
+
+
+class Bird extends LivingOrganism {
+    constructor () {
+        super();
+        this.eats = true;
+    }
+
+    printHabit () {
+        console.log(this.eats);
+    }
+}
+
+class Penguin extends Bird {
+    constructor() {
+        super();
+        this.habitat = "Antartica";
+    }
+
+    printPlaceofOrigin() {
+        console.log(this.habitat);
+    }
+} 
+
+const mypenguin = new Penguin();
+console.log(mypenguin);
+// ====> Penguin { breaths: true, eats: true, habitat: 'Antartica' }
+
+mypenguin.printPlaceofOrigin();  // ====> Antartica
+mypenguin.printHabit();   // ====> true
+mypenguin.isBreathing();   // ====> true
