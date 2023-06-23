64d93f8087e5a91868143239b2d50137bbbdff9a
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-23 15:33:58 +0530

    23-6-2023

--- JavaScript/Prototype.js
+++ JavaScript/Prototype.js
@@ -20,4 +20,30 @@ console.log(arr.___proto__.___proto__);
 console.log(obj.___proto__);
 
 
+let user = {
+    name : "Pawan",
+    sayHi : function () {
+        console.log(this.name);
+    }
+};
+
+let admin = {
+    role : "admin",
+    age : 30
+};
+
+console.log(user.name);    // ====> Pawan
+user.sayHi();   // ====> Pawan
+
+admin.__proto__ = user;
+console.log(admin.name);   // ====> Pawan
+
+admin.name = "Prashant"; 
+console.log(admin.name);   // ====> Prashant
+admin.sayHi();  // ====> Prashant
+
+console.log(user.name);   // ====> Pawan
+
+
+
 
