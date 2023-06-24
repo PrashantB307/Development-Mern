de90c7391f9cb36cfab2da8be7ecec81c7b109d3
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-24 11:27:44 +0530

    24-6-2023

--- JavaScript/Prototype.js
+++ JavaScript/Prototype.js
@@ -46,4 +46,20 @@ console.log(user.name);   // ====> Pawan
 
 
 
+// =================>  Prototype  Inherits  <===================
+// -------------------------------------------------------------
+
+let animal = {
+    eats: true
+};
+
+let rabbit = {
+    jumps: true
+};
+
+rabbit.__proto__ = animal; 
+
+console.log( rabbit.eats );    // ====> true
+console.log( rabbit.jumps );   // ====> true
+
 
