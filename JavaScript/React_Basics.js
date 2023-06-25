09ec9e1002d2b0ef4ad6cafb134977b44afa0ef7
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-25 15:48:41 +0530

    Basic Infomation about React

--- JavaScript/React_Basics.js
+++ JavaScript/React_Basics.js
@@ -25,3 +25,12 @@ bundled.exportedObj;
 //               age : 22
 //           }
 //        };
+
+
+// ======================>  Important Point  <========================
+//-----------------------------  React   -----------------------------
+
+
+//  In render() method we can not use if-else, so we use ternary operator.
+//  In React for writing JS code use => { } this bracket
+//  and for CSS styling use => {{ }} this bracket
