aa49b13a2f87eddcae68b921476c2184d9fe2642
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-26 16:32:54 +0530

    26-6-2023

--- JavaScript/HOF.js
+++ JavaScript/HOF.js
@@ -8,3 +8,17 @@ arr = arr.map(function (ele) {
 })
 
 console.log(arr);  //----> [ 1, 4, 9, 16 ]
+
+
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+//===================>   High Order Function  <===================
+//--------------------------  ( H.O.F )  -------------------------
+
+// Function which take other func. as argument.
+//                   or
+// Function which returns a func. is called HOF.
+
+
