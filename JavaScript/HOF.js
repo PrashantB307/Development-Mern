4a0c397acc4a2a21412ed9ceaff6a7acc3365009
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-06-27 19:52:24 +0530

    HOF

--- JavaScript/HOF.js
+++ JavaScript/HOF.js
@@ -37,3 +37,22 @@ let ans = fn();
 console.log(ans);       // ====> 7
 
 
+// Functional Programming ======>
+//-------------------------------
+
+const arr1 = [2, 4, 6, 8, 7, 9];
+
+// Calculate the Area of Squares
+let areaOfSquare = function(arr1) {
+    let res = [];
+    for(let i = 0; i < arr1.length; i++){
+        res.push(arr1[i] * arr1[i]);
+    }
+
+    return res;
+}
+
+console.log(areaOfSquare(arr1));     
+// ====> [ 4, 16, 36, 64, 49, 81 ]
+
+
