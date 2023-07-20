de5520a99b938e7db8ca1d7e711ed2ab2e3c7662
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-07-20 19:30:58 +0530

    :fire:  ToDo by use of React  :fire:

--- React/todo/src/Components/Todo.js
+++ React/todo/src/Components/Todo.js
@@ -36,6 +36,31 @@ export default class Todo extends Component {
     });
   }
 
+  render() {
+    console.log("Render method is Called");
+    return (  
+      // From here jsx starts
+      <div>
+        <input type='text' 
+        value={this.state.curTask} 
+        onChange={this.handleChange} 
+        />
+        <button onClick={this.handleSubmit}>
+          Submit
+        </button>
 
-  
+        {    
+        // {} used when need to write JS in jsx
+          this.state.tasks.map( (taskObj) => {
+            return (
+              <li key = {taskObj.id}>
+                <p>{taskObj.task}</p>
+                <button onClick={ () => { this.handleDeleteTasks(taskObj.id)} }>Delete</button>
+              </li>
+            );
+          })
+        }
+      </div>
+    )
+  }
 }
