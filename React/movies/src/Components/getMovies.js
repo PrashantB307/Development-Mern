2b2f87b6c0028504a59274b83a328269f80ca3b0
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-07-27 22:26:01 +0530

    27-6-2023

--- React/movies/src/Components/getMovies.js
+++ React/movies/src/Components/getMovies.js
@@ -1,7 +1,41 @@
 export const movies = {
     page: 1,
     results: [
-      
+      {
+        adult: false,
+        backdrop_path: "/6EdKBYkB1ssgGjc249ud1L55o8d.jpg",
+        genre_ids: [28, 878, 53],
+        id: 848278,
+        original_language: "en",
+        original_title: "Jurassic Hunt",
+        overview:
+          "Female adventurer Parker joins a crew of male trophy hunters in a remote wilderness park. Their goal: slaughter genetically recreated dinosaurs for sport using rifles, arrows, and grenades. After their guide is killed by raptors, the team tries to escape the park – but the hunters quickly become the hunted. Even worse, the park’s manager suspects Parker of being a spy and sends a hit squad after her. This battle’s about to become primitive!",
+        popularity: 4475.053,
+        poster_path: "/Czhr00kB8awffakEcQS5ON1ELm.jpg",
+        release_date: "2021-08-23",
+        title: "Jurassic Hunt",
+        video: false,
+        vote_average: 4.7,
+        vote_count: 50,
+      },
+      {
+        adult: false,
+        backdrop_path: "/nDLylQOoIazGyYuWhk21Yww5FCb.jpg",
+        genre_ids: [28, 12, 14],
+        id: 566525,
+        original_language: "en",
+        original_title: "Shang-Chi and the Legend of the Ten Rings",
+        overview:
+          "Shang-Chi must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.",
+        popularity: 3247.45,
+        poster_path: "/pS1XUGjC6ASC1kvDCP3OJnwjk1t.jpg",
+        release_date: "2021-09-01",
+        title: "Shang-Chi and the Legend of the Ten Rings",
+        video: false,
+        vote_average: 8,
+        vote_count: 691,
+      },
+     
     ],
     total_pages: 500,
     total_results: 10000,
