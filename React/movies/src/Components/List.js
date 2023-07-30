38d9c0c089a606738ec241b5e836634746dce62e
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-07-30 11:29:14 +0530

    30-7-2023

--- React/movies/src/Components/List.js
+++ React/movies/src/Components/List.js
@@ -2,3 +2,19 @@ import React, { Component } from "react";
 //import { movies } from './getMovies';
 import axios from "axios";
 import API_KEY from "../secrets";
+
+export default class List extends Component {
+  constructor() {
+    super();
+    // console.log("Constructor is called");
+    this.state = {
+      hover: "",
+      parr: [1],
+      currPage: 1,
+      movies: [],
+      favMov: [], // this will store the id of the movies added to favoures
+    };
+  }
+
+  
+}
