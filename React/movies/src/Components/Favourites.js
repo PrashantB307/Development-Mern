fd8b4015c41fe6b5fd0715ac80e6f0d1b41754c4
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-09 19:07:47 +0530

    9-8-2023

--- React/movies/src/Components/Favourites.js
+++ React/movies/src/Components/Favourites.js
@@ -13,6 +13,138 @@ export default class Favourites extends Component {
     };
   }
 
+  async componentDidMount() {
   
+    let results = JSON.parse(localStorage.getItem("movies"));
+    let genreId = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
+    let genreArr = [];
+    results.map((movieObj) => {
+      if (!genreArr.includes(genreId[movieObj.genre_ids[0]])) {
+        genreArr.push(genreId[movieObj.genre_ids[0]]);
+      }
+    });
+
+    genreArr.unshift("All Genre");
+    console.log(genreArr);
+    this.setState({
+      movies: [...results], //[{},{},{}]
+      genre: [...genreArr],
+    });
+  }
+
+  handleCurrGenre = (genre) => {
+    this.setState({
+      currGenre: genre,
+    });
+    // movies filter movies setstate
+  };
+
+  handleText = (e) => {
+    this.setState({
+      currText: e.target.value,
+    });
+  };
+
+  sortPopularityAsc = () => {
+    let allMovies = this.state.movies;
+    allMovies.sort((objA, objB) => {
+      return objA.popularity - objB.popularity;
+    });
+    this.setState({
+      movies: [...allMovies],
+    });
+  };
+
+  sortPopularityDesc = () => {
+    let allMovies = this.state.movies;
+    allMovies.sort((objA, objB) => {
+      return objB.popularity - objA.popularity;
+    });
+    this.setState({
+      movies: [...allMovies],
+    });
+  };
+  sortRatingAsc = () => {
+    let allMovies = this.state.movies;
+    allMovies.sort((objA, objB) => {
+      return objA.vote_average - objB.vote_average;
+    });
+    this.setState({
+      movies: [...allMovies],
+    });
+  };
+
+  sortRatingDesc = () => {
+    let allMovies = this.state.movies;
+    allMovies.sort((objA, objB) => {
+      return objB.vote_average - objA.vote_average;
+    });
+    this.setState({
+      movies: [...allMovies],
+    });
+  };
+
+  handlePageNum = (page) => {
+    this.setState({
+      currPage: page,
+    });
+  };
+
+  handleDelete = (id) => {
+    let newMovies = this.state.movies.filter((movieObj) => {
+      return movieObj.id !== id;
+    });
+    this.setState({
+      movies:[...newMovies]
+    })
+    localStorage.setItem("movies", JSON.stringify(newMovies));
+
+  }
+
+  render() {
+    let genreId = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
+
+    
+  }
 }
 
