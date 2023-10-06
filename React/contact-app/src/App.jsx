dbb0c0965375b22653391758f5e3b0f674736715
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-06 22:36:55 +0530

    6-10-2023

---
+++ React/contact-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "./config/firebase";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ContactCard from "./components/ContactCard";
+import AddAndUpdateContact from "./components/AddAndUpdateContact";
+import useDisclouse from "./hooks/useDisclouse";
+import NotFoundContact from "./components/NotFoundContact";
+
+
