5daeb41479fc3f6e559166b83b4075a363c1bbaf
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-10 09:09:09 +0530

    10-10-2023

--- React/contact-app/src/components/AddAndUpdateContact.jsx
+++ React/contact-app/src/components/AddAndUpdateContact.jsx
@@ -6,3 +6,25 @@ import { toast } from "react-toastify";
 import * as Yup from "yup";
 
 
+const contactSchemaValidation = Yup.object().shape({
+  Name: Yup.string().required("* Name is Required"),
+  Mobile_No: Yup.number().required("* Mobile No. is Required"),
+});
+
+const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
+  
+  const addContact = async (contact) => {
+    try {
+      const contactReff = collection(db, "contacts");
+      await addDoc(contactReff, contact);
+      onClose();
+      toast.success("Contact Added Successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+};
+
+export default AddAndUpdateContact;
