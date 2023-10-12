bafb5ebadd59ca1cc070f9fdc87fe2287a66ae47
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-12 17:02:07 +0530

    :fire:  :fire:   Contact Card   :fire:  :fire:

--- React/contact-app/src/components/ContactCard.jsx
+++ React/contact-app/src/components/ContactCard.jsx
@@ -26,7 +26,21 @@ const ContactCard = ({ contact }) => {
         key={contact.id}
         className="flex items-center justify-between rounded-lg bg-yellow p-2"
       >
-        
+        <div className="flex gap-1">
+          <HiOutlineUserCircle className="text-5xl text-orange " />
+          <div className="">
+            <h2 className="text-xl font-medium"> {contact.Name}</h2>
+            <p className="font-medium"> {contact.Mobile_No}</p>
+          </div>
+        </div>
+
+        <div className="flex text-3xl">
+          <RiEditCircleLine onClick={onOpen} className="cursor-pointer" />
+          <IoMdTrash
+            onClick={() => deleteContact(contact.id)}
+            className=" cursor-pointer text-orange"
+          />
+        </div>
       </div>
 
       <AddAndUpdateContact contact = {contact} isUpdate isOpen={isOpen} onClose={onClose} />
