ef420800a92dff72cabc9a71a7678f5728187cd7
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-14 20:55:54 +0530

    14-10-2023

---
+++ React/contact-app/src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import { createPortal } from "react-dom";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Modal = ({ onClose, isOpen, children }) => {
+  return createPortal(
+    <>
+      
+    </>,
+    document.getElementById("modal-root")
+  );
+};
+
+export default Modal;
