a6b90469ab68f9dede626dce8e9270bc146206ec
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-14 20:56:21 +0530

    :fire:  :fire:  Modal  :fire:  :fire:

--- React/contact-app/src/components/Modal.jsx
+++ React/contact-app/src/components/Modal.jsx
@@ -4,7 +4,16 @@ import { AiOutlineClose } from "react-icons/ai";
 const Modal = ({ onClose, isOpen, children }) => {
   return createPortal(
     <>
-      
+      {isOpen && (
+        <div className="absolute top-0 z-40 grid h-screen w-screen place-items-center backdrop-blur">
+          <div className="relative z-50 m-auto min-h-[200px] w-[400px] bg-white p-4">
+            <div className="flex justify-end">
+              <AiOutlineClose onClick={onClose} className="self-end text-2xl text-red-500" />
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
     </>,
     document.getElementById("modal-root")
   );
