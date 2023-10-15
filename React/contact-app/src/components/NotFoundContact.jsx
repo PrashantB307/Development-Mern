cd5439e5f387188990791c12f031f39e2f8d352d
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-15 17:19:55 +0530

    :fire: :fire:  Contact Not Found  :fire:  :fire:

---
+++ React/contact-app/src/components/NotFoundContact.jsx
@@ -0,0 +1,13 @@
+
+const NotFoundContact = () => {
+  return (
+    <div className="flex pt-20 items-center justify-center">
+        <img src="../../public/Hands Contact.png"/>
+        <h3 className=" text-2xl font-semibold text-white ">
+            Conatct Not Found
+        </h3>
+    </div>
+  )
+}
+
+export default NotFoundContact;
