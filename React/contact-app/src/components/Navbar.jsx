b34e30fe169a7b0048e5505930320043c894dae7
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-15 17:18:59 +0530

    :fire:  :fire:   Navbar  :fire:  :fire:

---
+++ React/contact-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="h-[60px] gap-2  bg-white my-4 rounded-lg 
+                    text-xl font-medium flex justify-center items-center">
+      <img src="/logos_firebase.svg" />
+      <h1>Firebase Contact App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
