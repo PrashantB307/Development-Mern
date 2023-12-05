f34408fa00198b0b4ec2f7d8064a280d33934ac2
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-05 23:31:28 +0530

    5-12-2023

--- React/shopping_cart/src/App.jsx
+++ React/shopping_cart/src/App.jsx
@@ -5,3 +5,18 @@ import {
   Navigate,
 } from "react-router-dom";
 
+import Home from "./pages/home/Home";
+import Order from "./pages/order/Order";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import NoPage from "./pages/nopage/NoPage";
+import MyState from "./context/data/myState";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import AddProduct from "./pages/admin/pages/AddProduct";
+import UpdateProduct from "./pages/admin/pages/UpdateProduct";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Allproducts from './pages/allproducts/AllProducts';
+
