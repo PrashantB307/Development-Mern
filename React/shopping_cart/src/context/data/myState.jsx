af6edcd0fd04f0aefda23cc89e6fcf8906a92bdc
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-16 00:49:46 +0530

    16-12-2023

--- React/shopping_cart/src/context/data/myState.jsx
+++ React/shopping_cart/src/context/data/myState.jsx
@@ -150,6 +150,30 @@ function myState(props) {
   }
 
 
+  //====================>    Order Product Data      <========================
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [order, setOrder] = useState([]);
+
+  const getOrderData = async () => {
+    setLoading(true)
+    try {
+      const result = await getDocs(collection(firedb, "order"))
+      const ordersArray = [];
+      result.forEach((doc) => {
+        ordersArray.push(doc.data());
+        setLoading(false)
+      });
+      setOrder(ordersArray);
+      // console.log(ordersArray)
+      setLoading(false);
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+
   
 }
 
