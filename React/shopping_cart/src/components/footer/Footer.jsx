5a9feda44c5ee49219d9ff10490f0da3a1bcc74c
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-19 00:33:00 +0530

    19-12-2023

--- React/shopping_cart/src/components/footer/Footer.jsx
+++ React/shopping_cart/src/components/footer/Footer.jsx
@@ -3,3 +3,15 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import myContext from "../../context/data/myContext";
 
+export default function Footer() {
+  const context = useContext(myContext);
+  const { mode } = context;
+  return (
+    <footer
+      className="text-gray-600 body-font bg-gray-300"
+      style={{
+        backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
+        color: mode === "dark" ? "white" : "",
+      }}
+    >
+ 
