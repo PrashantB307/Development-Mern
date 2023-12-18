db628cedf5023991d3ab7691e64316010db422f4
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-19 00:32:38 +0530

    19-12-2023

---
+++ React/shopping_cart/src/components/footer/Footer.jsx
@@ -0,0 +1,5 @@
+// import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import myContext from "../../context/data/myContext";
+
