4ea4f29a0e718c001e3c35388f5d6b744254e844
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-12-25 10:49:39 +0530

    Dashboard

---
+++ React/shopping_cart/src/pages/admin/dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import Layout from "../../../components/layout/Layout";
+import DashboardTab from "./DashboardTab";
+
+function Dashboard() {
+  
+return (
+  <Layout>
+      <section className="text-gray-600 body-font mt-10 mb-10">
+          <DashboardTab/>
+      </section>
+  </Layout>
+)
+}
+
+export default Dashboard
