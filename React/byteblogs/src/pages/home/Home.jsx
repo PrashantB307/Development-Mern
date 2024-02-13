c0602bf075851a019733e8e00f4449fd7c29bde8
PrashantB-307 <pb28.rkt@gmail.com>
2024-02-13 18:46:13 +0530

    Home

---
+++ React/byteblogs/src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import BlogPostCard from "../../components/blogPostCard/BlogPostCard"
+import HeroSection from "../../components/herosection/HeroSection"
+import Layout from "../../components/layout/Layout"
+
+
+function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+}, [])
+
+  return (
+    <Layout>
+      <HeroSection/>
+      <BlogPostCard/>
+    </Layout>
+  )
+}
+
+export default Home
