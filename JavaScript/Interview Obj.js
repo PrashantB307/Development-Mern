12be8f1f7d44b76e935f6b16020fc2c6f31dd98a
PrashantB-307 <pb28.rkt@gmail.com>
2024-10-03 20:34:02 +0530

    Interview Ques. related to JS

--- JavaScript/Interview Obj.js
+++ JavaScript/Interview Obj.js
@@ -561,5 +561,28 @@ console.log(x_x2);
 let x_x2 = 10;   //  error
 
 
-t
+//==========================================================
+
+
+console.log('Start');
+setTimeout( () => {
+    console.log('Timeout');
+}, 0);
+
+console.log('End');
+
+//   Start  -->   End   -->  Timeout 
+
+
+//===========================================================
+
+
+setTimeout( () => {
+    console.log('Timeout');
+}, 0);
+
+Promise.resolve().then( () => console.log('Promise'));
+console.log('End');
+
+//  End  -->  Promise  -->  Timeout
 
